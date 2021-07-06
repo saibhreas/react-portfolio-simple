3f2826797cd581a39cf02d026c1d1df4e76e61c9
siobhan <sr@saibhreas.com>
2021-07-06 09:17:35 -0400

    made changes with tutor, but now have to go to class.  Need to look into react pakages toget the work done

---
+++ simple-react-portfolio/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+        <h3 className="m-0" style={{ fontSize: '3rem' }}>
+         Saibhreas React Profile
+        </h1>
+        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+          
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
